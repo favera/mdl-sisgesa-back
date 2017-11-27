56ccaac229baf650728962c305d0ba46aa8f95c7
fvera <faa88ver@gmail.com>
2017-11-27 10:37:13 -0300

    first commit back

---
+++ server.js
@@ -0,0 +1,11 @@
+//Back server
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('mdldb.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+});
