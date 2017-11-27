e8e3c36e8b50c1c96363ce913d25d872ab3a4287
fvera <faa88ver@gmail.com>
2017-11-27 10:46:39 -0300

    edit server file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Back server
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('mdldb.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('mdldb.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-server.use(middlewares)
-server.use(router)
-server.listen(4000, () => {
-    console.log('JSON Server is running')
-});
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
